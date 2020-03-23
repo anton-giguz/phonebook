45617bcbe62cf0cda86e5088077d9d0cbe3387db
anton-giguz <61377952+anton-giguz@users.noreply.github.com>
2020-03-23 19:48:19 +0700

    Refactor search

--- src/main/java/phonebook/EntrySearch.java
+++ src/main/java/phonebook/EntrySearch.java
@@ -1,7 +1,9 @@
 package phonebook;
 
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 
 interface EntrySearch {
@@ -12,20 +14,33 @@ class EntrySearchImpl implements EntrySearch {
 
     @PersistenceContext
     private EntityManager em;
+    private CriteriaBuilder cb;
+    private Predicate pt;
+    private CriteriaQuery<Entry> cq;
+    private Root<Entry> root;
+
+    private void init() {
+        cb   = em.getCriteriaBuilder();
+        pt   = cb.isTrue(cb.literal(true));
+        cq   = cb.createQuery(Entry.class);
+        root = cq.from(Entry.class);
+    }
 
     public List<Entry> search(Entry entry) {
-        CriteriaBuilder cb     = em.getCriteriaBuilder();
-        CriteriaQuery<Entry> q = cb.createQuery(Entry.class);
-        Root<Entry> e          = q.from(Entry.class);
+        init();
 
-        Predicate t  = cb.isTrue(cb.literal(true));
-        Predicate p1 = entry.getLastName() .equals("") ? t : cb.equal(e.get("lastName"),  entry.getLastName());
-        Predicate p2 = entry.getFirstName().equals("") ? t : cb.equal(e.get("firstName"), entry.getFirstName());
-        Predicate p3 = entry.getPhone()    .equals("") ? t : cb.equal(e.get("phone"),     entry.getPhone());
+        String lastName  = entry.getLastName();
+        String firstName = entry.getFirstName();
+        String phone     = entry.getPhone();
 
-        q.select(e).where(p1, p2, p3);
-        q.orderBy(cb.asc(e.get("index")));
-        return em.createQuery(q).getResultList();
+        Predicate p1 = lastName .equals("") ? pt : cb.equal(root.get("lastName"),  lastName);
+        Predicate p2 = firstName.equals("") ? pt : cb.equal(root.get("firstName"), firstName);
+        Predicate p3 = phone    .equals("") ? pt : cb.equal(root.get("phone"),     phone);
+
+        cq.select(root).where(p1, p2, p3).orderBy(cb.asc(root.get("index")));
+
+        TypedQuery<Entry> tq = em.createQuery(cq);
+        return tq.getResultList();
     }
 
 }
